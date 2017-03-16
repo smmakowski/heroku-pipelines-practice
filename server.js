3fc637bf9cc451a72034f0fbaf9563edf326d827
smmakowski <makowski.sm@gmail.com>
2017-03-15 19:25:51 -0700

    Add basic server and package.json

---
+++ server.js
@@ -0,0 +1,22 @@
+var http = require('http');
+
+//define port
+const PORT = 8080; 
+
+//setup request handler
+function handleRequest(request, response) {
+	if (request.method === 'GET'){
+		response.end('GOT!')
+	}
+
+
+	response.end('It works!')
+}
+
+//create server
+var server = http.createServer(handleRequest);
+
+//create 
+server.listen(PORT, function(){
+	console.log('Server listening on http://localhost:' + PORT);
+});
